2e13d01795e212759d9b627dd8ff0ccfcc8c6165
Restyled.io <commits@restyled.io>
2023-06-20 09:47:05 +0000

    Restyled by clang-format

--- programs/priority-analyzer/tests/babel.js
+++ programs/priority-analyzer/tests/babel.js
@@ -1,11 +1,7 @@
-require("@babel/register")(
-{
-  "presets": ["@babel/preset-env"],
-  "plugins": 
-  [
-  [
-  "@babel/plugin-transform-runtime", 
+require("@babel/register")({
+  "presets" : [ "@babel/preset-env" ],
+  "plugins" : [ [
+    "@babel/plugin-transform-runtime",
 
-  ]
-],
+  ] ],
 });
