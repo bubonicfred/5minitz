3ce8baca090b79621a70d21f6bd49b38fa73b3b9
Restyled.io <commits@restyled.io>
2023-10-01 04:00:43 +0000

    Restyled by prettier

--- client/helpers/userTracker.js
+++ client/helpers/userTracker.js
@@ -1,14 +1,14 @@
-import {_} from "lodash";
-import {Meteor} from "meteor/meteor";
+import { _ } from "lodash";
+import { Meteor } from "meteor/meteor";
 
 const KEEP_ALIVE_INTERVAL_IN_MS = 30 * 1000;
 
 const getVisibilityKeys = () => {
   const keys = {
-    hidden : "visibilitychange",
-    webkitHidden : "webkitvisibilitychange",
-    mozHidden : "mozvisibilitychange",
-    msHidden : "msvisibilitychange",
+    hidden: "visibilitychange",
+    webkitHidden: "webkitvisibilitychange",
+    mozHidden: "mozvisibilitychange",
+    msHidden: "msvisibilitychange",
   };
   let stateKey, eventKey;
 
@@ -19,8 +19,8 @@ const getVisibilityKeys = () => {
     }
   }
   return {
-    stateKey : stateKey,
-    eventKey : eventKey,
+    stateKey: stateKey,
+    eventKey: eventKey,
   };
 };
 
@@ -32,7 +32,9 @@ const removeVisiblityListener = (listener) => {
   document.removeEventListener(getVisibilityKeys().eventKey, listener);
 };
 
-const isVisible = () => { return !document[getVisibilityKeys().stateKey]; };
+const isVisible = () => {
+  return !document[getVisibilityKeys().stateKey];
+};
 
 export class UserTracker {
   constructor(activeRoute) {
@@ -48,7 +50,9 @@ export class UserTracker {
     };
   }
 
-  _setActiveRoute() { Meteor.call("onlineUsers.enterRoute", this.activeRoute); }
+  _setActiveRoute() {
+    Meteor.call("onlineUsers.enterRoute", this.activeRoute);
+  }
 
   _clearActiveRoute() {
     Meteor.call("onlineUsers.leaveRoute", this.activeRoute);
@@ -63,8 +67,8 @@ export class UserTracker {
 
   _startTimer() {
     this.timerHandler = Meteor.setInterval(
-        _.bind(this._setActiveRoute, this),
-        KEEP_ALIVE_INTERVAL_IN_MS,
+      _.bind(this._setActiveRoute, this),
+      KEEP_ALIVE_INTERVAL_IN_MS,
     );
   }
 
@@ -80,5 +84,7 @@ export class UserTracker {
     window.removeEventListener("beforeunload", this.beforeUnloadListener);
   }
 
-  _stopTimer() { Meteor.clearInterval(this.timerHandler); }
+  _stopTimer() {
+    Meteor.clearInterval(this.timerHandler);
+  }
 }
