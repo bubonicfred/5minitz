031ecb3b2e6d2ed115fffd6fdca677216dc47ead
Restyled.io <commits@restyled.io>
2023-10-01 04:00:40 +0000

    Restyled by clang-format

--- imports/ldap/transformUser.js
+++ imports/ldap/transformUser.js
@@ -1,40 +1,42 @@
 let _ = require("lodash");
 
-module.exports = function (ldapSettings, userData) {
+module.exports = function(ldapSettings, userData) {
   ldapSettings.propertyMap = ldapSettings.propertyMap || {};
   const usernameAttribute =
-      ldapSettings.searchDn || ldapSettings.propertyMap.username || "cn",
-    longnameAttribute = ldapSettings.propertyMap.longname,
-    mailAttribute = ldapSettings.propertyMap.email || "mail";
+            ldapSettings.searchDn || ldapSettings.propertyMap.username || "cn",
+        longnameAttribute = ldapSettings.propertyMap.longname,
+        mailAttribute = ldapSettings.propertyMap.email || "mail";
 
   // userData.mail may be a string with one mail address or an array.
-  // Nevertheless we are only interested in the first mail address here - if there should be more...
+  // Nevertheless we are only interested in the first mail address here - if
+  // there should be more...
   let tmpEMail = userData[mailAttribute];
   if (Array.isArray(tmpEMail)) {
     tmpEMail = tmpEMail[0];
   }
   let tmpEMailArray = [
     {
-      address: tmpEMail,
-      verified: true,
-      fromLDAP: true,
+      address : tmpEMail,
+      verified : true,
+      fromLDAP : true,
     },
   ];
 
   let username = userData[usernameAttribute] || "";
 
   const whiteListedFields = ldapSettings.whiteListedFields || [];
-  const profileFields = whiteListedFields.concat(["dn"]);
+  const profileFields = whiteListedFields.concat([ "dn" ]);
 
   let user = {
-    createdAt: new Date(),
-    isInactive: false,
-    emails: tmpEMailArray,
-    username: username.toLowerCase(),
-    profile: _.pick(userData, _.without(profileFields, "mail")),
+    createdAt : new Date(),
+    isInactive : false,
+    emails : tmpEMailArray,
+    username : username.toLowerCase(),
+    profile : _.pick(userData, _.without(profileFields, "mail")),
   };
 
-  // copy over the LDAP user's long name from "cn" field to the meteor accounts long name field
+  // copy over the LDAP user's long name from "cn" field to the meteor accounts
+  // long name field
   if (longnameAttribute) {
     user.profile.name = userData[longnameAttribute];
   }
