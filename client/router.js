5807c888ea6de3c2f3557ff8cad259e9c533040b
Restyled.io <commits@restyled.io>
2023-10-05 06:50:15 +0000

    Restyled by prettier

--- client/router.js
+++ client/router.js
@@ -1,69 +1,82 @@
-import {Accounts} from "meteor/accounts-base";
-import {BlazeLayout} from "meteor/kadira:blaze-layout";
-import {FlowRouter} from "meteor/ostrio:flow-router-extra";
-import {i18n} from "meteor/universe:i18n";
+import { Accounts } from "meteor/accounts-base";
+import { BlazeLayout } from "meteor/kadira:blaze-layout";
+import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import { i18n } from "meteor/universe:i18n";
 
-import {FlashMessage} from "./helpers/flashMessage";
+import { FlashMessage } from "./helpers/flashMessage";
 
 // reset document title on route exit
 // To set a route specific title use a helper in the according template
-const resetDocumentTitle = function() { document.title = '4Minitz!' };
+const resetDocumentTitle = function () {
+  document.title = "4Minitz!";
+};
 
-FlowRouter.route(
-    '/', {action() { BlazeLayout.render('appLayout', {main : 'home'}); }});
+FlowRouter.route("/", {
+  action() {
+    BlazeLayout.render("appLayout", { main: "home" });
+  },
+});
 
 FlowRouter.route("/admin", {
-  name : "admin",
-  action() { BlazeLayout.render("appLayout", {main : "admin"}); },
+  name: "admin",
+  action() {
+    BlazeLayout.render("appLayout", { main: "admin" });
+  },
 });
 
 FlowRouter.route("/legalnotice", {
-  name : "legalNotice",
-  action() { BlazeLayout.render("legalNotice"); },
+  name: "legalNotice",
+  action() {
+    BlazeLayout.render("legalNotice");
+  },
 });
 
 FlowRouter.route("/verify-email/:token", {
-  name : "verify-email",
+  name: "verify-email",
   action(params) {
     Accounts.verifyEmail(params.token, (error) => {
       if (error) {
         new FlashMessage(
-            i18n.__("FlashMessages.verifyEMailErr"),
-            error.reason,
-            )
-            .show();
+          i18n.__("FlashMessages.verifyEMailErr"),
+          error.reason,
+        ).show();
       } else {
         FlowRouter.go("/");
         new FlashMessage(
-            "",
-            i18n.__("FlashMessages.verifyEMailOK"),
-            "alert-success",
-            )
-            .show();
+          "",
+          i18n.__("FlashMessages.verifyEMailOK"),
+          "alert-success",
+        ).show();
       }
     });
   },
 });
 
 FlowRouter.route("/reset-password/:token", {
-  name : "reset-password",
-  action() { BlazeLayout.render("resetPassword"); },
+  name: "reset-password",
+  action() {
+    BlazeLayout.render("resetPassword");
+  },
 });
 
 FlowRouter.route("/meetingseries/:_id", {
-  name : "meetingseries",
+  name: "meetingseries",
   action() {
-    BlazeLayout.render("appLayout", {main : "meetingSeriesDetails"});
+    BlazeLayout.render("appLayout", { main: "meetingSeriesDetails" });
   },
 });
 
 FlowRouter.route("/minutesedit/:_id", {
-  name : "minutesedit",
-  action() { BlazeLayout.render("appLayout", {main : "minutesedit"}); },
-  triggersExit : [ resetDocumentTitle ],
+  name: "minutesedit",
+  action() {
+    BlazeLayout.render("appLayout", { main: "minutesedit" });
+  },
+  triggersExit: [resetDocumentTitle],
 });
 
 FlowRouter.route("/topic/:_id", {
-  name : "topic",
-  action() { BlazeLayout.render("appLayout", {main : "topicViewOne"}); },
+  name: "topic",
+  action() {
+    BlazeLayout.render("appLayout", { main: "topicViewOne" });
+  },
 });
