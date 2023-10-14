d38ea32513fe6622a1f64228a25679953d43fe31
Restyled.io <commits@restyled.io>
2023-10-14 09:16:03 +0000

    style: format code with prettier

--- tests/performance/fixtures/generators/meeting-series-generator.js
+++ tests/performance/fixtures/generators/meeting-series-generator.js
@@ -1,8 +1,8 @@
-import {_} from "underscore";
+import { _ } from "underscore";
 
-import {DateHelper} from "../lib/date-helper";
-import {gMSV} from "../lib/gMSV";
-import {Random} from "../lib/random";
+import { DateHelper } from "../lib/date-helper";
+import { gMSV } from "../lib/gMSV";
+import { Random } from "../lib/random";
 
 export class MeetingSeriesGenerator {
   /**
@@ -18,20 +18,22 @@ export class MeetingSeriesGenerator {
 
   generate() {
     this.series = {
-      _id : Random.generateId(),
-      project : gMSV.generateMeetingSeriesValues().project,
-      name : gMSV.generateMeetingSeriesValues().name,
-      createdAt : new Date(),
-      lastMinutesDate : DateHelper.formatDateISO8601(new Date()),
-      visibleFor : [ this.user._id ],
-      availableLabels : [],
-      minutes : [],
-      additionalResponsibles : [],
+      _id: Random.generateId(),
+      project: gMSV.generateMeetingSeriesValues().project,
+      name: gMSV.generateMeetingSeriesValues().name,
+      createdAt: new Date(),
+      lastMinutesDate: DateHelper.formatDateISO8601(new Date()),
+      visibleFor: [this.user._id],
+      availableLabels: [],
+      minutes: [],
+      additionalResponsibles: [],
     };
     return this.series;
   }
 
-  addAllMinutes(minutes) { minutes.forEach(_.bind(this.addMinutes, this)); }
+  addAllMinutes(minutes) {
+    minutes.forEach(_.bind(this.addMinutes, this));
+  }
 
   addMinutes(aMinutes) {
     this.series.minutes.push(aMinutes._id);
