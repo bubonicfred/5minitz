5c1f4cfbba1190ab7aefda278b4d41a2a676aa30
Restyled.io <commits@restyled.io>
2024-05-08 06:02:33 +0000

    style: format code with clang-format

--- imports/label.js
+++ imports/label.js
@@ -1,20 +1,21 @@
-import { _ } from "lodash";
-import { Meteor } from "meteor/meteor";
-import { ColorHelper } from "./ColorHelper";
-import { MeetingSeries } from "./meetingseries";
+import {_} from "lodash";
+import {Meteor} from "meteor/meteor";
+
+import {ColorHelper} from "./ColorHelper";
+import {MeetingSeries} from "./meetingseries";
 
 export class Label {
   constructor(source) {
     if (!source) {
       throw new Meteor.Error(
-        "It is not allowed to create a Label without the source",
+          "It is not allowed to create a Label without the source",
       );
     }
 
     _.defaults(source, {
-      isDefaultLabel: false,
-      isDisabled: false,
-      color: "#e6e6e6",
+      isDefaultLabel : false,
+      isDisabled : false,
+      color : "#e6e6e6",
     });
 
     const nameAndColor = Label._separateNameAndColor(source.name);
@@ -30,12 +31,12 @@ export class Label {
 
   static _separateNameAndColor(nameAndColorStr) {
     const nameAndColor = nameAndColorStr.match(
-      /(.*)(#([a-f\d][a-f\d][a-f\d]){1,2})$/,
+        /(.*)(#([a-f\d][a-f\d][a-f\d]){1,2})$/,
     );
     if (nameAndColor && nameAndColor.length > 2) {
       return {
-        name: nameAndColor[1],
-        color: nameAndColor[2],
+        name : nameAndColor[1],
+        color : nameAndColor[2],
       };
     }
 
@@ -46,7 +47,8 @@ export class Label {
     parentMeetingSeries = Label._createParentMeetingSeries(parentMeetingSeries);
 
     const labelDoc = parentMeetingSeries.findLabel(labelId);
-    if (labelDoc) return new Label(labelDoc);
+    if (labelDoc)
+      return new Label(labelDoc);
     return null;
   }
 
@@ -59,64 +61,51 @@ export class Label {
     parentMeetingSeries = Label._createParentMeetingSeries(parentMeetingSeries);
 
     const labelDoc = parentMeetingSeries.findLabelByName(labelName);
-    if (labelDoc) return new Label(labelDoc);
+    if (labelDoc)
+      return new Label(labelDoc);
     return null;
   }
 
   static findLabelsContainingSubstring(
-    parentMeetingSeries,
-    name,
-    caseSensitive,
+      parentMeetingSeries,
+      name,
+      caseSensitive,
   ) {
     parentMeetingSeries = Label._createParentMeetingSeries(parentMeetingSeries);
 
     const labelDoc = parentMeetingSeries.findLabelContainingSubstr(
-      name,
-      caseSensitive,
+        name,
+        caseSensitive,
     );
-    if (labelDoc) return labelDoc;
+    if (labelDoc)
+      return labelDoc;
     return null;
   }
 
   static _createParentMeetingSeries(parentMeetingSeries) {
     if (typeof parentMeetingSeries === "string") {
       return new MeetingSeries(parentMeetingSeries);
-    } else if (
-      Object.prototype.hasOwnProperty.call(parentMeetingSeries, "findLabel")
-    ) {
+    } else if (Object.prototype.hasOwnProperty.call(parentMeetingSeries,
+                                                    "findLabel")) {
       return parentMeetingSeries;
     }
 
     throw new Meteor.Error("Invalid parent meeting series");
   }
 
-  getId() {
-    return this._labelDoc._id;
-  }
+  getId() { return this._labelDoc._id; }
 
-  setName(name) {
-    this._labelDoc.name = name;
-  }
+  setName(name) { this._labelDoc.name = name; }
 
-  getName() {
-    return this._labelDoc.name;
-  }
+  getName() { return this._labelDoc.name; }
 
-  getColor() {
-    return this._labelDoc.color;
-  }
+  getColor() { return this._labelDoc.color; }
 
-  setColor(color) {
-    this._labelDoc.color = color;
-  }
+  setColor(color) { this._labelDoc.color = color; }
 
-  hasDarkBackground() {
-    return ColorHelper.isDarkColor(this.getColor());
-  }
+  hasDarkBackground() { return ColorHelper.isDarkColor(this.getColor()); }
 
-  getDocument() {
-    return this._labelDoc;
-  }
+  getDocument() { return this._labelDoc; }
 
   save(parentMeetingSeries) {
     parentMeetingSeries = Label._createParentMeetingSeries(parentMeetingSeries);
@@ -128,8 +117,8 @@ export class Label {
   _checkLabelColor() {
     if (!ColorHelper.isValidHexColorString(this.getColor())) {
       throw new Meteor.Error(
-        "invalid-color",
-        "Label color must be a valid hex code",
+          "invalid-color",
+          "Label color must be a valid hex code",
       );
     }
   }
