8fbb79dea9b1db3b2f56bcd139ba3c12ebf28c1e
Restyled.io <commits@restyled.io>
2024-05-15 06:51:49 +0000

    style: format code with clang-format

--- tests/end2end/helpers/E2EMinutes.js
+++ tests/end2end/helpers/E2EMinutes.js
@@ -1,7 +1,7 @@
-import { E2EGlobal } from "./E2EGlobal";
-import { E2EApp } from "./E2EApp";
-import { E2EMeetingSeries } from "./E2EMeetingSeries";
-import { E2EMinutesParticipants } from "./E2EMinutesParticipants";
+import {E2EApp} from "./E2EApp";
+import {E2EGlobal} from "./E2EGlobal";
+import {E2EMeetingSeries} from "./E2EMeetingSeries";
+import {E2EMinutesParticipants} from "./E2EMinutesParticipants";
 
 export class E2EMinutes {
   /**
@@ -53,11 +53,12 @@ export class E2EMinutes {
    *
    * @param confirmDialog should the dialog be confirmed automatically
    *                      default: true
-   *        processFinalize is true, when you want to proceed finalizing Minutes without participants
+   *        processFinalize is true, when you want to proceed finalizing Minutes
+   * without participants
    */
   static finalizeCurrentMinutesWithoutParticipants(
-    confirmDialog,
-    processFinalize,
+      confirmDialog,
+      processFinalize,
   ) {
     browser.waitForVisible("#btn_finalizeMinutes");
     E2EGlobal.clickWithRetry("#btn_finalizeMinutes");
