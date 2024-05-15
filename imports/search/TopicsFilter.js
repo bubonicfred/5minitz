75648fb8b2107e6a9083a4d4bcf970f9898b989d
Restyled.io <commits@restyled.io>
2024-05-15 23:37:50 +0000

    style: format code with prettier

--- imports/search/TopicsFilter.js
+++ imports/search/TopicsFilter.js
@@ -1,8 +1,8 @@
-import {_} from "lodash";
-import {Meteor} from "meteor/meteor";
+import { _ } from "lodash";
+import { Meteor } from "meteor/meteor";
 
-import {TOPIC_KEYWORDS} from "./FilterKeywords";
-import {ItemsFilter} from "./ItemsFilter";
+import { TOPIC_KEYWORDS } from "./FilterKeywords";
+import { ItemsFilter } from "./ItemsFilter";
 
 export class TopicsFilter {
   constructor() {
@@ -20,9 +20,11 @@ export class TopicsFilter {
 
     this.isCaseSensitive = parser.isCaseSensitive();
     return docs.filter((doc) => {
-      return (this.docMatchesSearchTokens(doc, parser.getSearchTokens()) &&
-              this.docMatchesLabelTokens(doc, parser.getLabelTokens()) &&
-              this.docMatchesFilterTokens(doc, parser.getFilterTokens()));
+      return (
+        this.docMatchesSearchTokens(doc, parser.getSearchTokens()) &&
+        this.docMatchesLabelTokens(doc, parser.getLabelTokens()) &&
+        this.docMatchesFilterTokens(doc, parser.getFilterTokens())
+      );
     });
   }
 
@@ -32,12 +34,9 @@ export class TopicsFilter {
       const subject = this._toUpper(doc.subject);
 
       const hasMatchingInfoItems =
-          this.itemsFilter
-              .filterWithParams(doc.infoItems, this.isCaseSensitive,
-                                [
-                                  token,
-                                ])
-              .length > 0;
+        this.itemsFilter.filterWithParams(doc.infoItems, this.isCaseSensitive, [
+          token,
+        ]).length > 0;
 
       if (!subject.includes(token) && !hasMatchingInfoItems) {
         return false;
@@ -46,18 +45,19 @@ export class TopicsFilter {
     return true;
   }
 
-  _toUpper(str) { return this.isCaseSensitive ? str : str.toUpperCase(); }
+  _toUpper(str) {
+    return this.isCaseSensitive ? str : str.toUpperCase();
+  }
 
   docMatchesLabelTokens(doc, labelTokens) {
     for (const token of labelTokens) {
-      const hasMatchingInfoItems = this.itemsFilter
-                                       .filterWithParams(
-                                           doc.infoItems,
-                                           this.isCaseSensitive,
-                                           [],
-                                           [ token ],
-                                           )
-                                       .length > 0;
+      const hasMatchingInfoItems =
+        this.itemsFilter.filterWithParams(
+          doc.infoItems,
+          this.isCaseSensitive,
+          [],
+          [token],
+        ).length > 0;
       if (!hasMatchingInfoItems) {
         return false;
       }
@@ -70,32 +70,32 @@ export class TopicsFilter {
       const filter = filterTokens[i];
 
       switch (filter.key) {
-      case TOPIC_KEYWORDS.IS.key: {
-        if (!this.constructor._docMatchesKeyword_IS(doc, filter.value)) {
-          return false;
+        case TOPIC_KEYWORDS.IS.key: {
+          if (!this.constructor._docMatchesKeyword_IS(doc, filter.value)) {
+            return false;
+          }
+          break;
         }
-        break;
-      }
-      case TOPIC_KEYWORDS.HAS.key: {
-        if (!this._docMatchesKeyword_HAS(doc, filter.value)) {
-          return false;
+        case TOPIC_KEYWORDS.HAS.key: {
+          if (!this._docMatchesKeyword_HAS(doc, filter.value)) {
+            return false;
+          }
+          break;
         }
-        break;
-      }
-      case TOPIC_KEYWORDS.USER.key: {
-        if (!this._docMatchesKeywords_USER(doc, filter)) {
-          return false;
+        case TOPIC_KEYWORDS.USER.key: {
+          if (!this._docMatchesKeywords_USER(doc, filter)) {
+            return false;
+          }
+          break;
         }
-        break;
-      }
-      case TOPIC_KEYWORDS.DO.key: {
-        break;
-      }
-      default:
-        throw new Meteor.Error(
+        case TOPIC_KEYWORDS.DO.key: {
+          break;
+        }
+        default:
+          throw new Meteor.Error(
             "illegal-state",
             `Unknown filter keyword: ${filter.key}`,
-        );
+          );
       }
     }
 
@@ -106,52 +106,54 @@ export class TopicsFilter {
     if (!doc.responsibles) {
       return false;
     }
-    const respStr =
-        doc.responsibles.reduce((acc, resp) => { return acc + resp; }, "");
-    return ((filter.ids &&
-             _.intersection(doc.responsibles, filter.ids).length > 0) ||
-            (filter.value &&
-             this._toUpper(respStr).includes(this._toUpper(filter.value))));
+    const respStr = doc.responsibles.reduce((acc, resp) => {
+      return acc + resp;
+    }, "");
+    return (
+      (filter.ids && _.intersection(doc.responsibles, filter.ids).length > 0) ||
+      (filter.value &&
+        this._toUpper(respStr).includes(this._toUpper(filter.value)))
+    );
   }
 
   static _docMatchesKeyword_IS(doc, value) {
     switch (value) {
-    case "uncompleted":
-    case "open":
-      return doc.isOpen;
-    case "completed":
-    case "closed":
-      return !doc.isOpen;
-    case "new":
-      return doc.isNew;
-    default:
-      throw new Meteor.Error(
+      case "uncompleted":
+      case "open":
+        return doc.isOpen;
+      case "completed":
+      case "closed":
+        return !doc.isOpen;
+      case "new":
+        return doc.isNew;
+      default:
+        throw new Meteor.Error(
           "illegal-state",
           `Unknown filter value: ${value}`,
-      );
+        );
     }
   }
 
   _docMatchesKeyword_HAS(doc, value) {
     switch (value) {
-    case "item":
-      return doc.infoItems.length > 0;
-    case "info":
-    case "action": {
-      const items = this.itemsFilter.filterWithParams(
+      case "item":
+        return doc.infoItems.length > 0;
+      case "info":
+      case "action": {
+        const items = this.itemsFilter.filterWithParams(
           doc.infoItems,
           this.isCaseSensitive,
           [],
           [],
-          [ {key : "is", value} ],
-      );
-      return items.length > 0;
-    }
-    default:
-      throw new Meteor.Error(
+          [{ key: "is", value }],
+        );
+        return items.length > 0;
+      }
+      default:
+        throw new Meteor.Error(
           "illegal-state",
           `Unknown filter value: ${value}`,
-      );
+        );
     }
   }
 }
