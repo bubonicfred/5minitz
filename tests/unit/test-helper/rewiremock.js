38d9371b122461cc0bc46dfb1f86f7977faae8a0
Ciaran Welch <bubonicfred@gmail.com>
2024-05-18 18:47:16 +1000

    update actionitem test, fix rewiremock export

--- tests/unit/test-helper/rewiremock.js
+++ tests/unit/test-helper/rewiremock.js
@@ -3,4 +3,4 @@ import rewiremock from 'rewiremock';
 // settings
 // ....
 rewiremock.overrideEntryPoint(module); // this is important. This command is "transfering" this module parent to rewiremock
-export { rewiremock }
+export { rewiremock };
