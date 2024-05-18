d272411871f0edb4136faf8d4d8c63fc4c70b156
Ciaran Welch <bubonicfred@gmail.com>
2024-05-18 19:41:13 +1000

    Add node plugin to rewiremock

--- tests/unit/test-helper/rewiremock.js
+++ tests/unit/test-helper/rewiremock.js
@@ -1,5 +1,5 @@
 // rewiremock.es6.js
-import rewiremock from 'rewiremock';
+import rewiremock from 'rewiremock/node';
 // settings
 // ....
 rewiremock.overrideEntryPoint(module); // this is important. This command is "transfering" this module parent to rewiremock
