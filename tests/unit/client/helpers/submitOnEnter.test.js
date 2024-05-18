ed033de60519690047d9f4472ca20a28a99f81c4
Ciaran Welch <bubonicfred@gmail.com>
2024-05-18 21:34:17 +1000

    import jq?

--- tests/unit/client/helpers/submitOnEnter.test.js
+++ tests/unit/client/helpers/submitOnEnter.test.js
@@ -7,7 +7,7 @@ const jQueryOnStub = sinon.stub();
 const $ = sinon.stub().returns({
   on: jQueryOnStub,
 });
-
+import "meteor/jquery";
 const submitOnEnter = rewiremock.proxy('#root/client/helpers/submitOnEnter', {
   "meteor/jquery": { $, "@noCallThru": true },
   "meteor/underscore": { _, "@noCallThru": true },
