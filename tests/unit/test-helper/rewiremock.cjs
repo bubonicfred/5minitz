37b81d451c51a99010683c966e23ddf4687f74f0
Ciaran Welch <bubonicfred@gmail.com>
2024-05-19 12:41:54 +1000

    Test tweak to rewiremock

--- tests/unit/test-helper/rewiremock.cjs
+++ tests/unit/test-helper/rewiremock.cjs
@@ -2,5 +2,6 @@
 const rewiremock = require('rewiremock/node');
 // settings
 // ....
-rewiremock.overrideEntryPoint(module); // this is important. This command is "transfering" this module parent to rewiremock
+// rewiremock.overrideEntryPoint(module);
+// this is important. This command is "transfering" this module parent to rewiremock
 module.exports = rewiremock;
