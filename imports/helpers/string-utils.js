9f366aea1ffdc7419f4f829dcac93b548cde5127
Ciaran Welch <bubonicfred@gmail.com>
2024-05-20 21:16:19 +1000

    docs: Improve docs

--- imports/helpers/string-utils.js
+++ imports/helpers/string-utils.js
@@ -2,6 +2,14 @@
  * Utility class for performing string manipulation operations.
  */
 export class StringUtils {
+  /**
+   * Removes all occurrences of a substring from a given string.
+   *
+   * @param {string} string - The original string.
+   * @param {string} substring - The substring to be removed.
+   * @returns {string} - The modified string with all occurrences of the
+   *     substring removed.
+   */
   static eraseSubstring(string, substring) {
     string = string.replace(`${substring} `, "");
     string = string.replace(` ${substring}`, "");
