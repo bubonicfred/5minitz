ae91e67d775e2e8c60b81592847a6ddb384430cd
Ciaran Welch <bubonicfred@gmail.com>
2024-05-22 01:45:21 +1000

    Add uncommited line

--- tests/unit/imports/Topic.test.js
+++ tests/unit/imports/Topic.test.js
@@ -80,7 +80,6 @@ const { InfoItemFactory } = proxyquire("../../../imports/InfoItemFactory", {
 const { Topic } = proxyquire("../../../imports/topic", {
   "meteor/meteor": { Meteor, "@noCallThru": true },
   "meteor/random": { Random, "@noCallThru": true },
-  "meteor/underscore": { _, "@noCallThru": true },
   "/imports/helpers/subElements": SubElements,
   "./label": { Label, "@noCallThru": true },
   "./infoitem": { InfoItem, "@noCallThru": true },
