d06d5d3d761995a203da266ae8551e9403b796cb
Ciaran Welch <bubonicfred@gmail.com>
2024-05-24 10:14:02 +1000

    Fix Deepsource ignore comments

--- tests/unit/imports/helpers/subElements.test.js
+++ tests/unit/imports/helpers/subElements.test.js
@@ -5,7 +5,7 @@ import { subElementsHelper } from "../../../../imports/helpers/subElements";
 describe("subElementsHelper", function () {
   // skipcq JS-0241
   describe("#findIndexById", function () {
-    // skipq JS-0241
+    // skipcq JS-0241
     it("returns undefined if an empty list is given", function () {
       const list = [];
       const id = "someId";
@@ -14,7 +14,7 @@ describe("subElementsHelper", function () {
 
       expect(result).to.be.undefined;
     });
- // skipq JS-0241
+ // skipcq JS-0241
     it("returns undefined if the given id is not found", function () {
       const list = [{}, {}, {}];
       const id = "someId";
@@ -23,7 +23,7 @@ describe("subElementsHelper", function () {
 
       expect(result).to.be.undefined;
     });
- // skipq JS-0241
+ // skipcq JS-0241
     it("returns the index of the element with the given id", function () {
       const id = "someId";
       const list = [{}, { _id: "someId" }, {}];
@@ -32,7 +32,7 @@ describe("subElementsHelper", function () {
 
       expect(result).to.equal(1);
     });
-    // skipq JS-0241
+    // skipcq JS-0241
     it("returns the index of the element with the given id it first encounters", function () {
       const id = "someId";
       const list = [{}, { _id: id }, { _id: id }];
