6a555c4f0d6f819637c71f322b1734788337045a
Restyled.io <commits@restyled.io>
2024-05-26 07:14:35 +0000

    style: format code with prettier

--- server/mongoBackup.js
+++ server/mongoBackup.js
@@ -1,4 +1,4 @@
-import {spawn} from "child_process";
+import { spawn } from "child_process";
 import mongoUri from "mongo-uri";
 
 /**
--- tests/unit/server/mongoBackup.test.js
+++ tests/unit/server/mongoBackup.test.js
@@ -1,32 +1,36 @@
-import {expect} from "chai";
+import { expect } from "chai";
 import proxyquire from "proxyquire";
 import sinon from "sinon";
 
-const spawn = sinon.stub().returns({on : sinon.spy()});
+const spawn = sinon.stub().returns({ on: sinon.spy() });
 
-const {backupMongo} = proxyquire("../../../server/mongoBackup", {
-  child_process : {spawn, "@noCallThru" : true},
+const { backupMongo } = proxyquire("../../../server/mongoBackup", {
+  child_process: { spawn, "@noCallThru": true },
 });
 
 // skipcq: JS-0241
-describe("mongoBackup", function() {
+describe("mongoBackup", function () {
   // skipcq: JS-0241
-  describe("#backupMongo", function() {
+  describe("#backupMongo", function () {
     // skipcq: JS-0241
-    beforeEach(function() { spawn.resetHistory(); });
+    beforeEach(function () {
+      spawn.resetHistory();
+    });
     // skipcq: JS-0241
-    it("uses mongodump to create a backup", function() {
-      backupMongo("mongodb://user:password@localhost:1234/database",
-                  "outputdir");
+    it("uses mongodump to create a backup", function () {
+      backupMongo(
+        "mongodb://user:password@localhost:1234/database",
+        "outputdir",
+      );
 
       const firstCall = spawn.args[0];
       const command = firstCall[0];
       const parameters = firstCall[1].join(";");
 
       expect(command).to.equal("mongodump");
-      expect(parameters)
-          .to.equal(
-              "-h;localhost:1234;-u;user;-p;password;-d;database;-o;outputdir");
+      expect(parameters).to.equal(
+        "-h;localhost:1234;-u;user;-p;password;-d;database;-o;outputdir",
+      );
     });
   });
 });
