cc5295ef6c7d05dc9adcde93e11664c2c6f2ed2e
Restyled.io <commits@restyled.io>
2024-05-30 06:37:43 +0000

    style: format code with prettier

--- imports/server_side_templates/global_helpers.js
+++ imports/server_side_templates/global_helpers.js
@@ -1,5 +1,5 @@
-import {Markdown} from "meteor/perak:markdown";
-import {Spacebars} from "meteor/spacebars";
+import { Markdown } from "meteor/perak:markdown";
+import { Spacebars } from "meteor/spacebars";
 import sanitizeHtml from "sanitize-html"; // Import the sanitize-html package
 // Replace <p> tags with <br> to preserve email layout
 const PARAGRAPH_REGEX = /<p>(.*?)<\/p>/gi;
@@ -28,7 +28,8 @@ export const GlobalHelpers = {
       html = Markdown(text);
     } catch (e) {
       console.error(
-          `Could( not convert the following markdown to html: ${text}`);
+        `Could( not convert the following markdown to html: ${text}`,
+      );
       throw new Error(`Markdown conversion failed: ${e.message}`);
     }
 
@@ -37,7 +38,7 @@ export const GlobalHelpers = {
 
   doctype() {
     const dt =
-        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+      '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
     return Spacebars.SafeString(dt);
   },
 
