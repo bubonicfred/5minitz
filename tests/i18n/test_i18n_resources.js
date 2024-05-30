dbd58c14f635c32f559e44ab15f97d6419b6d477
Ciaran Welch <bubonicfred@gmail.com>
2024-05-30 13:26:09 +1000

    chore: Update js-yaml to version 4.1.0

--- tests/i18n/test_i18n_resources.js
+++ tests/i18n/test_i18n_resources.js
@@ -127,7 +127,7 @@ function checkCodeUsage(extension, keyPattern) {
 // Read and parse YAML file to JS object
 let yaml_doc = undefined;
 try {
-  yaml_doc = yaml.safeLoad(fs.readFileSync(en_yaml, "utf8"));
+  yaml_doc = yaml.load(fs.readFileSync(en_yaml, "utf8"));
 } catch (e) {
   console.log(e);
   anyErrorExitCodeToShell = 10;
