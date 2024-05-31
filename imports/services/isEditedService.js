e0f7d9f14904ef81517d4212f916ea0c3ff7e3b4
Ciaran Welch <bubonicfred@gmail.com>
2024-06-01 00:28:20 +1000

    Fix import?

--- imports/services/isEditedService.js
+++ imports/services/isEditedService.js
@@ -1,5 +1,4 @@
 import { Meteor } from "meteor/meteor";
-import { check } from "meteor/check";
 import { MeetingSeriesSchema } from "../collections/meetingseries.schema";
 import { MinutesSchema } from "../collections/minutes.schema";
 import { MeetingSeries } from "../meetingseries";
