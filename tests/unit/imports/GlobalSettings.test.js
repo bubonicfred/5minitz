9fab1397b3400f09c63c0593c93c878ab419c2dc
Restyled.io <commits@restyled.io>
2024-06-16 04:41:42 +0000

    style: format code with clang-format

--- tests/unit/imports/GlobalSettings.test.js
+++ tests/unit/imports/GlobalSettings.test.js
@@ -1,15 +1,17 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import proxyquire from "proxyquire";
 import sinon from "sinon";
 
 class MeteorError {}
 const Meteor = {
-  Error: MeteorError,
-  absoluteUrl: (path, config) => {
-    if (!path) path = "";
+  Error : MeteorError,
+  absoluteUrl : (path, config) => {
+    if (!path)
+      path = "";
 
     if (config?.rootUrl) {
-      if (path !== "") path = `/${path}`;
+      if (path !== "")
+        path = `/${path}`;
       return config.rootUrl + path;
     }
     return path;
@@ -17,98 +19,105 @@ const Meteor = {
 };
 
 const LdapSettings = {
-  publish: sinon.stub(),
+  publish : sinon.stub(),
 };
 
-const { GlobalSettings } = proxyquire(
-  "../../../imports/config/GlobalSettings",
-  {
-    "meteor/meteor": { Meteor, "@noCallThru": true },
-    "/imports/config/LdapSettings": { LdapSettings, "@noCallThru": true },
-  },
+const {GlobalSettings} = proxyquire(
+    "../../../imports/config/GlobalSettings",
+    {
+      "meteor/meteor" : {Meteor, "@noCallThru" : true},
+      "/imports/config/LdapSettings" : {LdapSettings, "@noCallThru" : true},
+    },
 );
+// skipcq: JS-0241
+describe("GlobalSettings", function() {
   // skipcq: JS-0241
-describe("GlobalSettings", function () {
-    // skipcq: JS-0241
-  beforeEach("publish public settings", function () {
+  beforeEach("publish public settings", function() {
     Meteor.settings = require("../../../settings_sample.json");
     GlobalSettings.publishSettings();
   });
   // skipcq: JS-0241
-  describe("#getRootUrl", function () {
-      // skipcq: JS-0241
-    it("returns the correct value", function () {
+  describe("#getRootUrl", function() {
+    // skipcq: JS-0241
+    it("returns the correct value", function() {
       expect(GlobalSettings.getRootUrl()).to.equal(Meteor.settings.ROOT_URL);
     });
-  // skipcq: JS-0241
-    it("returns an empty string if property is not set", function () {
+    // skipcq: JS-0241
+    it("returns an empty string if property is not set", function() {
       delete Meteor.settings.ROOT_URL;
       expect(GlobalSettings.getRootUrl()).to.equal("");
     });
-  // skipcq: JS-0241
-    it("does not fail if no settings file give", function () {
+    // skipcq: JS-0241
+    it("does not fail if no settings file give", function() {
       Meteor.settings = {};
       expect(GlobalSettings.getRootUrl()).to.equal("");
     });
   });
   // skipcq: JS-0241
-  describe("#isTrustedIntranetInstallation", function () {
-      // skipcq: JS-0241
-    it("returns the correct value", function () {
-      expect(GlobalSettings.isTrustedIntranetInstallation()).to.equal(
-        Meteor.settings.trustedIntranetInstallation,
-      );
+  describe("#isTrustedIntranetInstallation", function() {
+    // skipcq: JS-0241
+    it("returns the correct value", function() {
+      expect(GlobalSettings.isTrustedIntranetInstallation())
+          .to.equal(
+              Meteor.settings.trustedIntranetInstallation,
+          );
     });
-  // skipcq: JS-0241
-    it("returns false if property is not set", function () {
+    // skipcq: JS-0241
+    it("returns false if property is not set", function() {
       delete Meteor.settings.trustedIntranetInstallation;
       expect(GlobalSettings.isTrustedIntranetInstallation()).to.be.false;
     });
-  // skipcq: JS-0241
-    it("does not fail if no settings file give", function () {
+    // skipcq: JS-0241
+    it("does not fail if no settings file give", function() {
       Meteor.settings = {};
       expect(GlobalSettings.isTrustedIntranetInstallation()).to.be.false;
     });
   });
   // skipcq: JS-0241
-  describe("#getDefaultEmailSenderAddress", function () {
-      // skipcq: JS-0241
-    it("returns the default email sender address", function () {
-      expect(GlobalSettings.getDefaultEmailSenderAddress()).to.equal(
-        Meteor.settings.email.defaultEMailSenderAddress,
-      );
+  describe("#getDefaultEmailSenderAddress", function() {
+    // skipcq: JS-0241
+    it("returns the default email sender address", function() {
+      expect(GlobalSettings.getDefaultEmailSenderAddress())
+          .to.equal(
+              Meteor.settings.email.defaultEMailSenderAddress,
+          );
     });
-  // skipcq: JS-0241
-    it("returns the alternative address of the current user if property is left empty", function () {
-      Meteor.settings.email.defaultEMailSenderAddress = "";
-      const alternative = "alternativeSenderAddress";
-      expect(GlobalSettings.getDefaultEmailSenderAddress(alternative)).to.equal(
-        alternative,
-      );
-    });
-  // skipcq: JS-0241
-    it("returns fallback sender address if no alternative address is given", function () {
-      Meteor.settings.email.defaultEMailSenderAddress = "";
-      expect(GlobalSettings.getDefaultEmailSenderAddress()).to.equal(
-        Meteor.settings.email.fallbackEMailSenderAddress,
-      );
-    });
-  // skipcq: JS-0241
-    it("throws exception if fallback sender address required but not given", function () {
-      Meteor.settings.email.defaultEMailSenderAddress = "";
-      delete Meteor.settings.email.fallbackEMailSenderAddress;
-      let exceptionThrown;
-      try {
-        GlobalSettings.getDefaultEmailSenderAddress();
-        exceptionThrown = false;
-      } catch (e) {
-        exceptionThrown = e instanceof MeteorError;
-      }
+    // skipcq: JS-0241
+    it("returns the alternative address of the current user if property is left empty",
+       function() {
+         Meteor.settings.email.defaultEMailSenderAddress = "";
+         const alternative = "alternativeSenderAddress";
+         expect(GlobalSettings.getDefaultEmailSenderAddress(alternative))
+             .to.equal(
+                 alternative,
+             );
+       });
+    // skipcq: JS-0241
+    it("returns fallback sender address if no alternative address is given",
+       function() {
+         Meteor.settings.email.defaultEMailSenderAddress = "";
+         expect(GlobalSettings.getDefaultEmailSenderAddress())
+             .to.equal(
+                 Meteor.settings.email.fallbackEMailSenderAddress,
+             );
+       });
+    // skipcq: JS-0241
+    it("throws exception if fallback sender address required but not given",
+       function() {
+         Meteor.settings.email.defaultEMailSenderAddress = "";
+         delete Meteor.settings.email.fallbackEMailSenderAddress;
+         let exceptionThrown;
+         try {
+           GlobalSettings.getDefaultEmailSenderAddress();
+           exceptionThrown = false;
+         } catch (e) {
+           exceptionThrown = e instanceof MeteorError;
+         }
 
-      expect(exceptionThrown, "Method did not throw exception").to.be.true;
-    });
-  // skipcq: JS-0241
-    it("throws exception if property is not set", function () {
+         expect(exceptionThrown, "Method did not throw exception").to.be.true;
+       });
+    // skipcq: JS-0241
+    it("throws exception if property is not set", function() {
       delete Meteor.settings.email.defaultEMailSenderAddress;
 
       let exceptionThrown;
@@ -123,73 +132,77 @@ describe("GlobalSettings", function () {
     });
   });
   // skipcq: JS-0241
-  describe("#isEMailDeliveryEnabled", function () {
-      // skipcq: JS-0241
-    it("returns the correct value", function () {
-      expect(GlobalSettings.isEMailDeliveryEnabled()).to.equal(
-        Meteor.settings.email.enableMailDelivery,
-      );
+  describe("#isEMailDeliveryEnabled", function() {
+    // skipcq: JS-0241
+    it("returns the correct value", function() {
+      expect(GlobalSettings.isEMailDeliveryEnabled())
+          .to.equal(
+              Meteor.settings.email.enableMailDelivery,
+          );
     });
-  // skipcq: JS-0241
-    it("returns false if property is not set", function () {
+    // skipcq: JS-0241
+    it("returns false if property is not set", function() {
       delete Meteor.settings.email.enableMailDelivery;
       GlobalSettings.publishSettings();
       expect(GlobalSettings.isEMailDeliveryEnabled()).to.be.false;
     });
-  // skipcq: JS-0241
-    it("does not fail if no settings file give", function () {
+    // skipcq: JS-0241
+    it("does not fail if no settings file give", function() {
       Meteor.settings = {};
       expect(GlobalSettings.isEMailDeliveryEnabled()).to.be.false;
     });
   });
   // skipcq: JS-0241
-  describe("#getMailDeliverer", function () {
-      // skipcq: JS-0241
-    it("returns the correct value", function () {
-      expect(GlobalSettings.getMailDeliverer()).to.equal(
-        Meteor.settings.email.mailDeliverer,
-      );
+  describe("#getMailDeliverer", function() {
+    // skipcq: JS-0241
+    it("returns the correct value", function() {
+      expect(GlobalSettings.getMailDeliverer())
+          .to.equal(
+              Meteor.settings.email.mailDeliverer,
+          );
     });
-  // skipcq: JS-0241
-    it("returns smtp if property is not set", function () {
+    // skipcq: JS-0241
+    it("returns smtp if property is not set", function() {
       delete Meteor.settings.email.mailDeliverer;
       expect(GlobalSettings.getMailDeliverer()).to.equal("smtp");
     });
-  // skipcq: JS-0241
-    it("does not fail if no settings file give", function () {
+    // skipcq: JS-0241
+    it("does not fail if no settings file give", function() {
       Meteor.settings = {};
       expect(GlobalSettings.getMailDeliverer()).to.equal("smtp");
     });
   });
   // skipcq: JS-0241
-  describe("#getSMTPMailUrl", function () {
-      // skipcq: JS-0241
-    it("returns the correct value", function () {
-      expect(GlobalSettings.getSMTPMailUrl()).to.equal(
-        Meteor.settings.email.smtp.mailUrl,
-      );
+  describe("#getSMTPMailUrl", function() {
+    // skipcq: JS-0241
+    it("returns the correct value", function() {
+      expect(GlobalSettings.getSMTPMailUrl())
+          .to.equal(
+              Meteor.settings.email.smtp.mailUrl,
+          );
     });
-  // skipcq: JS-0241
-    it("returns an empty string if property is not set", function () {
+    // skipcq: JS-0241
+    it("returns an empty string if property is not set", function() {
       delete Meteor.settings.email.smtp.mailUrl;
       expect(GlobalSettings.getSMTPMailUrl()).to.equal("");
     });
-  // skipcq: JS-0241
-    it("does not fail if no settings file give", function () {
+    // skipcq: JS-0241
+    it("does not fail if no settings file give", function() {
       Meteor.settings = {};
       expect(GlobalSettings.getSMTPMailUrl()).to.equal("");
     });
   });
   // skipcq: JS-0241
-  describe("#getMailgunSettings", function () {
-      // skipcq: JS-0241
-    it("returns the correct value", function () {
-      expect(GlobalSettings.getMailgunSettings()).to.equal(
-        Meteor.settings.email.mailgun,
-      );
+  describe("#getMailgunSettings", function() {
+    // skipcq: JS-0241
+    it("returns the correct value", function() {
+      expect(GlobalSettings.getMailgunSettings())
+          .to.equal(
+              Meteor.settings.email.mailgun,
+          );
     });
-  // skipcq: JS-0241
-    it("throws exception if property is not set", function () {
+    // skipcq: JS-0241
+    it("throws exception if property is not set", function() {
       delete Meteor.settings.email.mailgun;
 
       let exceptionThrown;
--- tests/unit/imports/priority.test.js
+++ tests/unit/imports/priority.test.js
@@ -1,88 +1,89 @@
-import { expect } from "chai";
-import { AssertHelper } from "../test-helper/assert-helper";
-import sinon from "sinon";
+import {expect} from "chai";
 import proxyquire from "proxyquire";
+import sinon from "sinon";
+
+import {AssertHelper} from "../test-helper/assert-helper";
 
 const EXPECTED_PRIORITY_MAP = {
-  1: "1 - High",
-  2: "2",
-  3: "3 - Medium",
-  4: "4",
-  5: "5 - Low",
+  1 : "1 - High",
+  2 : "2",
+  3 : "3 - Medium",
+  4 : "4",
+  5 : "5 - Low",
 };
 
 const i18n = {
-  setLocale: sinon.stub(),
-  getLocale: sinon.stub(),
-  __: sinon.stub(),
+  setLocale : sinon.stub(),
+  getLocale : sinon.stub(),
+  __ : sinon.stub(),
 };
 
-const { Priority } = proxyquire("../../../imports/priority", {
-  "meteor/universe:i18n": { i18n, "@noCallThru": true },
+const {Priority} = proxyquire("../../../imports/priority", {
+  "meteor/universe:i18n" : {i18n, "@noCallThru" : true},
 });
-  // skipcq: JS-0241
+// skipcq: JS-0241
 describe("Priority", function() {
-    // skipcq: JS-0241
+  // skipcq: JS-0241
   describe("#constructor", function() {
-      // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should assign the given value as a property", function() {
       const value = 2;
       const prio = new Priority(value);
       expect(prio.value).to.equal(value);
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should convert a numeric string value to an integer", function() {
       const value = "2";
       const prio = new Priority(value);
       expect(prio.value).to.equal(parseInt(value, 10));
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should throw an exception for values below 1", function() {
       AssertHelper.shouldThrow(
-        () => new Priority(0),
-        "Constructor should throw an exception for the value 0",
+          () => new Priority(0),
+          "Constructor should throw an exception for the value 0",
       );
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should throw an exception for values over 5", function() {
       AssertHelper.shouldThrow(
-        () => new Priority(6),
-        "Constructor should throw an exception for the value 6",
+          () => new Priority(6),
+          "Constructor should throw an exception for the value 6",
       );
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should throw an exception for values of an invalid type", function() {
       AssertHelper.shouldThrow(
-        () => new Priority("b"),
-        "Constructor should throw an exception for the value of type string",
+          () => new Priority("b"),
+          "Constructor should throw an exception for the value of type string",
       );
     });
   });
   // skipcq: JS-0241
   describe("#toString", function() {
-      // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should return the string representation of the value", function() {
       const prio = new Priority(2);
       expect(prio.toString()).to.equal(EXPECTED_PRIORITY_MAP[2]);
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should throw for an invalid value", function() {
       const prio = new Priority(3);
       prio.value = 7;
       AssertHelper.shouldThrow(
-        () => prio.toString(),
-        "should throw for the value 7",
+          () => prio.toString(),
+          "should throw for the value 7",
       );
     });
   });
   // skipcq: JS-0241
   describe("#GET_DEFAULT_PRIORITY", function() {
-      // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should return an object of type Priority", function() {
       const defaultPrio = Priority.GET_DEFAULT_PRIORITY();
       expect(defaultPrio instanceof Priority).to.be.true;
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should return priority with value 3", function() {
       const defaultPrio = Priority.GET_DEFAULT_PRIORITY();
       expect(defaultPrio.value).to.equal(3);
@@ -90,12 +91,12 @@ describe("Priority", function() {
   });
   // skipcq: JS-0241
   describe("#GET_PRIORITIES", function() {
-      // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should return an array with five elements", function() {
       const prios = Priority.GET_PRIORITIES();
       expect(prios).to.have.length(5);
     });
-  // skipcq: JS-0241
+    // skipcq: JS-0241
     it("should return an array of Priorities", function() {
       const prios = Priority.GET_PRIORITIES();
       prios.forEach((prio) => expect(prio instanceof Priority).to.be.true);
